0ee4bb497e2e79a5102793d3bddf64ab54941a07
WasgehtPro <ben.becker@sign-group.de>
2024-05-28 19:03:51 +0200

    item system done baby

--- Items/Pipe/pipe.gd
+++ Items/Pipe/pipe.gd
@@ -1,43 +1,63 @@
-extends Node2D
+extends RigidBody2D
 
-const PIPE = preload("res://Items/Pipe/pipe.tscn")
-@export var player: CharacterBody2D
+var iManager = preload("res://ModuleScripts/ItemManager.gd").new()
+const bluePrint = preload("res://Items/Pipe/pipe.tscn")
+
+@onready var throwTimer = $ThrowTimer
+@onready var hitBox = $HitBox/MainCollision
+
 @onready var sprite = $Sprite
-var equipped = false
-@onready var collision = $Collision
-@onready var pipe = $"."
 
-func Use():
+@export var damage = 20
+var equipped = false
+var flying = false
+
+func use():
+	hitBox.disabled = false
 	var swing1 = create_tween()
 	swing1.tween_property(self, "rotation", deg_to_rad(90), .05)
-	await  swing1.finished
+	await swing1.finished
 	var swing2 = create_tween()
 	swing2.tween_property(self, "rotation", deg_to_rad(0), .5)
+	await swing2.finished
+	hitBox.disabled = true
+
+func onThrowStart():
+	hitBox.disabled = false
+
+func onThrowStop():
+	flying = true
+	throwTimer.start()
+	
+func _on_throw_timer_timeout():
+	if linear_velocity.length() < 30:
+		hitBox.disabled = true
+		flying = false
+		throwTimer.stop()
 	
 func drop(transform, dir):
-	queue_free()
-	var item := PIPE.instantiate() as RigidBody2D
-	item.global_transform = transform
-	item.apply_force(dir * 2000)
-	get_tree().get_current_scene().add_child(item)
+	iManager.drop(self, bluePrint, transform, dir)
 	
 func onEquip():
-	collision.disabled = true
+	$Collision.disabled = true
 	sprite.play("highlighted")
+	equipped = true
 
 func pickUp():
-	queue_free()
-	return PIPE.instantiate()
+	return iManager.pickUp(self, bluePrint)
 
 func _on_highlight_range_mouse_entered():
 	if not equipped:
-		sprite.play("highlighted")
+		iManager.highlight(sprite, equipped)
 
 func _on_highlight_range_mouse_exited():
 	if not equipped:
-		sprite.play("default")
-
+		iManager.unHighlight(sprite, equipped)
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Enemy"):
-		body.hit(20)
+		body.hit(damage)
+
+func _on_highlight_range_body_entered(body):
+	if body.is_in_group("Enemy") and flying:
+		body.hit(damage)
